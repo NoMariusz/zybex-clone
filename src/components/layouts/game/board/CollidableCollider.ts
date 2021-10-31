45b5e26be83f3e76bf6b1f04a5285601f4ec7050
NoMariusz <nomariusz27@gmail.com>
2021-10-31 10:09:33 +0100

    Fixed CollidableCollider
    Now works for all sized elements

--- src/components/layouts/game/board/CollidableCollider.ts
+++ src/components/layouts/game/board/CollidableCollider.ts
@@ -5,15 +5,24 @@ export default class CollidableCollider {
 
   checkCollision(element1: Collidable, element2: Collidable): boolean {
     /**
-     * check if two rectangle elements collide, by checking for every 4 corners of element1
-     * if they are in element's 2 area
-     * ! elemet1 should be that smaller element, to detect collisions correct
+     * check if two rectangle elements collide
+     * by checking if any corner of any element is in other element's area
      */
 
-    if (!this.validateElements(element1, element2))
-      throw new Error(
-        "Element1 should be not so much bigger that element2 to detect collisions correct!"
-      );
+    return (
+      this.checkIfElem1InElem2(element1, element2) ||
+      this.checkIfElem1InElem2(element2, element1)
+    );
+  }
+
+  private checkIfElem1InElem2(
+    element1: Collidable,
+    element2: Collidable
+  ): boolean {
+    /**
+     * check if two rectangle elements collide, by checking for every 4 corners of element1
+     * if they are in element's 2 area
+     */
 
     const { x: x1, y: y1 } = element1.position;
     const { x: x2, y: y2 } = element2.position;
@@ -27,13 +36,6 @@ export default class CollidableCollider {
       (xx1 >= x2 && xx1 <= xx2 && yy1 >= y2 && yy1 <= yy2) ||
       (x1 >= x2 && x1 <= xx2 && yy1 >= y2 && yy1 <= yy2) ||
       (xx1 >= x2 && xx1 <= xx2 && y1 >= y2 && y1 <= yy2)
-    );
-  }
-
-  private validateElements(element1: Collidable, element2: Collidable) {
-    return (
-      element1.size.height <= element2.size.height * 2 ||
-      element1.size.width <= element2.size.height * 2
     );
   }
 }
