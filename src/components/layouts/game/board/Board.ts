7707f2078f2bf84666632c30102b02b22f5fd6b6
NoMariusz <mariusz2727@vp.pl>
2021-11-10 12:07:10 +0100

    Fixex calculating player attack speed

--- src/components/layouts/game/board/Board.ts
+++ src/components/layouts/game/board/Board.ts
@@ -3,10 +3,11 @@ import pressedKeys from "../../../controls/pressedKeys";
 import { Position, Renderable } from "../../../interfaces";
 import Renderer from "../../../rendering/Renderer";
 import {
-  BOARD_HEIGHT,
-  BOARD_WIDTH,
-  MAX_ATTACK_SPEED_MULTIPLIER,
-  PLAYER_SPEED,
+    BOARD_HEIGHT,
+    BOARD_WIDTH,
+    MAX_ATTACK_SPEED_MULTIPLIER,
+    PLAYER_SPEED,
+    Weapons,
 } from "../constants";
 import EnemyManager from "../enemy/EnemyManager";
 import Player from "../player/Player";
@@ -17,168 +18,196 @@ import EnemySection from "../enemy/EnemySection";
 import Bullet from "../bullets/Bullet";
 
 export default class Board implements Renderable {
-  /* Handle board placed things, relations between enemy and player,
+    /* Handle board placed things, relations between enemy and player,
   like collisions, or calculating shot attack speed */
-  player: Player;
+    player: Player;
 
-  playfieldManager: PlayfieldManager;
-  enemyManager: EnemyManager;
-  collidableCollider: CollidableCollider;
-  pickupsManager: PickupsManager;
+    playfieldManager: PlayfieldManager;
+    enemyManager: EnemyManager;
+    collidableCollider: CollidableCollider;
+    pickupsManager: PickupsManager;
 
-  constructor(player: Player, enemyManager: EnemyManager) {
-    this.player = player;
-    this.playfieldManager = new PlayfieldManager(player);
-    this.enemyManager = enemyManager;
-    this.collidableCollider = new CollidableCollider();
-    this.pickupsManager = new PickupsManager();
-  }
-
-  render() {
-    this.movePlayer();
-    this.playfieldManager.render();
-    this.pickupsManager.render();
-    this.collidePlayer();
-    this.collideEnemies();
-    this.modifyAttackSpeed();
-  }
-
-  //player moves
-
-  movePlayer() {
-    // not move player when is locked
-    if (this.player.locked) {
-      return;
+    constructor(player: Player, enemyManager: EnemyManager) {
+        this.player = player;
+        this.playfieldManager = new PlayfieldManager(player);
+        this.enemyManager = enemyManager;
+        this.collidableCollider = new CollidableCollider();
+        this.pickupsManager = new PickupsManager();
     }
-    // get pos after move
-    const newPos = this.getPlayerNewPos();
-    // restrict player move to be at board area
-    const restrictedPos = this.restrictPlayerMove(newPos);
-    //make that move
-    this.player.position = restrictedPos;
-  }
 
-  getPlayerNewPos(): Position {
-    const velocity: Position = {
-      x:
-        Number(pressedKeys[Keys.LEFT]) * -1 +
-        Number(pressedKeys[Keys.RIGHT]) * 1,
-      y: Number(pressedKeys[Keys.UP]) * -1 + Number(pressedKeys[Keys.DOWN]) * 1,
-    };
-    return {
-      x: this.player.position.x + velocity.x * PLAYER_SPEED,
-      y: this.player.position.y + velocity.y * PLAYER_SPEED,
-    };
-  }
-
-  restrictPlayerMove(pos: Position) {
-    return {
-      x: Math.min(Math.max(pos.x, 0), BOARD_WIDTH - this.player.size.width),
-      y: Math.min(Math.max(pos.y, 0), BOARD_HEIGHT - this.player.size.height),
-    };
-  }
-
-  // not playfield collisions
-
-  collidePlayer() {
-    this.collidePlayerWithEnemies();
-    this.collidePlayerWithPickups();
-  }
-
-  collidePlayerWithEnemies() {
-    for (const enemyColl of this.enemyManager.collidablesWithPlayer) {
-      const collide = this.collidableCollider.checkCollision(
-        enemyColl,
-        this.player
-      );
-      if (collide) this.player.takeDamage();
+    render() {
+        this.movePlayer();
+        this.playfieldManager.render();
+        this.pickupsManager.render();
+        this.collidePlayer();
+        this.collideEnemies();
+        this.modifyAttackSpeed();
     }
-  }
 
-  collidePlayerWithPickups() {
-    for (const pickup of this.pickupsManager.pickups) {
-      const collide = this.collidableCollider.checkCollision(
-        pickup,
-        this.player
-      );
-      if (collide) {
-        this.player.onPickup(pickup);
-        this.pickupsManager.clearPickup(pickup);
-      }
-    }
-  }
+    //player moves
 
-  collideEnemies() {
-    if (this.enemyManager.activeEnemy == null) return;
-
-    for (const bullet of this.player.shotManager.bullets) {
-      let destroyBullet = false;
-
-      for (const enemyCollidable of this.enemyManager
-        .collidablesWithPlayerBullets) {
-        const collide = this.collidableCollider.checkCollision(
-          bullet,
-          enemyCollidable
-        );
-        if (collide) {
-          if (enemyCollidable instanceof EnemySection)
-            this.onEnemyCollide(enemyCollidable, bullet);
-          destroyBullet = true;
+    movePlayer() {
+        // not move player when is locked
+        if (this.player.locked) {
+            return;
         }
-      }
-      // destroy bullet after check collisions with all collidables
-      // so bullet can hit simultaneously many enemy sections
-      if (destroyBullet) {
-        bullet.destroy();
-      }
-    }
-  }
-
-  onEnemyCollide(enemySection: EnemySection, bullet: Bullet) {
-    enemySection.takeDamage(bullet.damage);
-    // detect if enemy died after got shoot
-    if (!enemySection.live) {
-      this.player.onEnemyDie();
-      this.pickupsManager.onEnemyDie(enemySection.position);
-    }
-  }
-
-  // calculating attack speed multiplier
-
-  modifyAttackSpeed() {
-    const enemiesOnLine = this.getEnemiesOnLine();
-    if (enemiesOnLine.length <= 0) {
-      this.player.shotManager.shotSpeedMultiplier = 1;
-      return;
+        // get pos after move
+        const newPos = this.getPlayerNewPos();
+        // restrict player move to be at board area
+        const restrictedPos = this.restrictPlayerMove(newPos);
+        //make that move
+        this.player.position = restrictedPos;
     }
 
-    const s = this.calcAttackSpeed(enemiesOnLine);
-    this.player.shotManager.shotSpeedMultiplier = s;
-  }
+    getPlayerNewPos(): Position {
+        const velocity: Position = {
+            x:
+                Number(pressedKeys[Keys.LEFT]) * -1 +
+                Number(pressedKeys[Keys.RIGHT]) * 1,
+            y:
+                Number(pressedKeys[Keys.UP]) * -1 +
+                Number(pressedKeys[Keys.DOWN]) * 1,
+        };
+        return {
+            x: this.player.position.x + velocity.x * PLAYER_SPEED,
+            y: this.player.position.y + velocity.y * PLAYER_SPEED,
+        };
+    }
 
-  private getEnemiesOnLine() {
-    return this.enemyManager.activeSections.filter(
-      (e) =>
-        Math.abs(
-          e.position.y +
-            e.size.height / 2 -
-            (this.player.position.y + this.player.size.height / 2)
-        ) < 25
-    );
-  }
+    restrictPlayerMove(pos: Position) {
+        return {
+            x: Math.min(
+                Math.max(pos.x, 0),
+                BOARD_WIDTH - this.player.size.width
+            ),
+            y: Math.min(
+                Math.max(pos.y, 0),
+                BOARD_HEIGHT - this.player.size.height
+            ),
+        };
+    }
 
-  private calcAttackSpeed(enemiesOnLine: EnemySection[]) {
-    const minDistance = enemiesOnLine.reduce(
-      (prev, current) =>
-        prev <= this.calcDistance(current) ? prev : this.calcDistance(current),
-      Infinity
-    );
-    const speedMultiplier =
-      minDistance == Infinity ? 1 : Math.sqrt(BOARD_WIDTH / minDistance);
-    return Math.min(speedMultiplier, MAX_ATTACK_SPEED_MULTIPLIER);
-  }
+    // not playfield collisions
 
-  private calcDistance(enemy: EnemySection) {
-    return Math.abs(enemy.position.x - this.player.position.x);
-  }
+    collidePlayer() {
+        this.collidePlayerWithEnemies();
+        this.collidePlayerWithPickups();
+    }
+
+    collidePlayerWithEnemies() {
+        for (const enemyColl of this.enemyManager.collidablesWithPlayer) {
+            const collide = this.collidableCollider.checkCollision(
+                enemyColl,
+                this.player
+            );
+            if (collide) this.player.takeDamage();
+        }
+    }
+
+    collidePlayerWithPickups() {
+        for (const pickup of this.pickupsManager.pickups) {
+            const collide = this.collidableCollider.checkCollision(
+                pickup,
+                this.player
+            );
+            if (collide) {
+                this.player.onPickup(pickup);
+                this.pickupsManager.clearPickup(pickup);
+            }
+        }
+    }
+
+    collideEnemies() {
+        if (this.enemyManager.activeEnemy == null) return;
+
+        for (const bullet of this.player.shotManager.bullets) {
+            let destroyBullet = false;
+
+            for (const enemyCollidable of this.enemyManager
+                .collidablesWithPlayerBullets) {
+                const collide = this.collidableCollider.checkCollision(
+                    bullet,
+                    enemyCollidable
+                );
+                if (collide) {
+                    if (enemyCollidable instanceof EnemySection)
+                        this.onEnemyCollide(enemyCollidable, bullet);
+                    destroyBullet = true;
+                }
+            }
+            // destroy bullet after check collisions with all collidables
+            // so bullet can hit simultaneously many enemy sections
+            if (destroyBullet) {
+                bullet.destroy();
+            }
+        }
+    }
+
+    onEnemyCollide(enemySection: EnemySection, bullet: Bullet) {
+        enemySection.takeDamage(bullet.damage);
+        // detect if enemy died after got shoot
+        if (!enemySection.live) {
+            this.player.onEnemyDie();
+            this.pickupsManager.onEnemyDie(enemySection.position);
+        }
+    }
+
+    // calculating attack speed multiplier
+
+    modifyAttackSpeed() {
+        // don't modify attack speed for 8way weapon
+        const speed =
+            this.player.weapon.type == Weapons.EightWay
+                ? 1
+                : this.calcAttackSpeed();
+        this.player.shotManager.shotSpeedMultiplier = speed;
+    }
+
+    private getEnemiesOnLine() {
+        return this.enemyManager.activeSections.filter(
+            (e) =>
+                Math.abs(
+                    e.position.y +
+                        e.size.height / 2 -
+                        (this.player.position.y + this.player.size.height / 2)
+                ) < 25
+        );
+    }
+
+    private calcAttackSpeed() {
+        const enemiesOnLine = this.getEnemiesOnLine();
+        if (enemiesOnLine.length <= 0) {
+            return this.calcAttackSpeedForRightEdge();
+        }
+
+        return this.calcAttackSpeedFromEnemies(enemiesOnLine);
+    }
+
+    private calcAttackSpeedFromEnemies(enemiesOnLine: EnemySection[]) {
+        const minDistance = enemiesOnLine.reduce(
+            (prev, current) =>
+                prev <= this.calcDistance(current)
+                    ? prev
+                    : this.calcDistance(current),
+            Infinity
+        );
+        return this.calcAttackSpeedFromDistance(minDistance);
+    }
+
+    private calcAttackSpeedForRightEdge() {
+        const distanceToRight = BOARD_WIDTH - this.player.position.x;
+        return this.calcAttackSpeedFromDistance(distanceToRight);
+    }
+
+    private calcDistance(enemy: EnemySection) {
+        return Math.abs(enemy.position.x - this.player.position.x);
+    }
+
+    private calcAttackSpeedFromDistance(distance: number) {
+        const speedMultiplier =
+            distance == Infinity
+                ? 1
+                : MAX_ATTACK_SPEED_MULTIPLIER * (1 - distance / BOARD_WIDTH);
+        return Math.max(speedMultiplier, 1);
+    }
 }
